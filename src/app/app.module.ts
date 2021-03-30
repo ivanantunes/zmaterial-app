616b7ef54f69fa232926ba1237cd7f75cf51dbe4
Ivan Antunes <ivanantnes75@gmail.com>
2021-03-29 22:41:28 -0300

    Adicionado libs novas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ZModalModule } from 'zmaterial';
+import { GlobalModule } from './global.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,11 @@ import { ZModalModule } from 'zmaterial';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    GlobalModule,
     ZModalModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/global.module.ts
@@ -0,0 +1,85 @@
+// Angular
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// Angular Material
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+
+// Toast
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+    declarations: [],
+    imports: [
+        // Angular
+        CommonModule,
+
+        // Http
+        HttpClientModule,
+
+        // Flex Layout
+        FlexLayoutModule,
+
+        // Forms
+        ReactiveFormsModule,
+        FormsModule,
+
+        // Angular Material
+        MatIconModule,
+        MatToolbarModule,
+        MatListModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatTooltipModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+
+        // Toast
+        ToastrModule.forRoot(),
+    ],
+    exports: [
+        // Flex Layout
+        FlexLayoutModule,
+
+        // Forms
+        ReactiveFormsModule,
+        FormsModule,
+
+        // Angular Material
+        MatIconModule,
+        MatToolbarModule,
+        MatListModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatTooltipModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+
+        // Toast
+        ToastrModule,
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class GlobalModule { }
